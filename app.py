4856d4085330c41aa11958a46bfc103ebd077999
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2022-09-28 10:31:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -105,8 +105,6 @@ if uploaded_file is not None:
         df_summary['Total Hours'] = df_summary['Unavailability'] + df_summary['Work Time (HH:MM)']
         df_summary = df_summary.set_index('Name')
         
-        
-        
         # overall total
         norm_total = df_summary["Normal Hours"].sum()
         unav_total = df_summary["Unavailability"].sum()
@@ -121,6 +119,7 @@ if uploaded_file is not None:
         # reindex to ensure all index values are present
         nameindex = ['A/L','A/L BH', 'E', 'L', 'LD','N','Ten','SuE','SuL','SuLD','SuN','SD-Mand','SD-NonMand','Other PD','MIL','MD']
         df_summary = df_summary.reindex(nameindex)
+        df_summary.fillna(0)
         
         # contracted
         tot_cont = df_summary.loc['E']['Total Hours'] + df_summary.loc['L']['Total Hours'] + df_summary.loc['LD']['Total Hours'] + df_summary.loc['N']['Total Hours']  + df_summary.loc['Ten']['Total Hours']
